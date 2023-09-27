fed74342f68eb3264d314170acb6587b423cacf6
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-09-27 15:52:23 +0200

    Import image with the correct size

--- src/main/webapp/plugins/cryptpad.js
+++ src/main/webapp/plugins/cryptpad.js
@@ -1,11 +1,26 @@
 Draw.loadPlugin(function(ui) {
+
+	const getImageSize = function(url) {
+		return new Promise((resolve) => {
+			var img = document.createElement("img");
+			img.src = url;
+			img.onload = function() {
+				img.style.visibility = 'hidden';
+				document.body.appendChild(img);
+				resolve({width: img.clientWidth, height: img.clientHeight});
+				document.body.removeChild(img);
+			}
+		});
+	}
+
 	// Adds resource for action
 	mxResources.parse('cryptPadImport=Import image from CryptPad...');
 
 	// Adds action
 	ui.actions.addAction('cryptPadImport', async function() {
 		const result = await window.parent.APP.addImage();
-		ui.importFile(result.url, result.fileType,10,10,8,8, result.name);
+		const size = await getImageSize(result.url);
+		ui.importFile(result.url, result.fileType, 0, 0, size.width, size.height, result.name);
 	});
 	
 	var menu = ui.menus.get('importFrom');
